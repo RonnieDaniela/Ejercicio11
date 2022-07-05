278dacacc55eda5f58fb5de4201e0654358b60f4
Daniela <fer.dany.et12@gmail.com>
2022-07-05 00:19:56 -0300

    Ejercicio11

---
+++ js/main.js
@@ -0,0 +1,17 @@
+js>
+let hojaEstilo = 0
+
+function cambiarCss() {
+    if (hojaEstilo == 0) {
+        document.getElementById('css').href = 'cssprueba/retro.css';
+        hojaEstilo = 1;
+    }
+    else if (hojaEstilo == 1) {
+        document.getElementById('css').href = 'cssprueba/futuro.css';
+        hojaEstilo = 2;
+    }
+    else if (hojaEstilo == 2) {
+        document.getElementById('css').href = 'cssprueba/style.css';
+        hojaEstilo = 0;
+    }
+}
