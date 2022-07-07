0ec9b3daecb80f520eff523e3b4b848fc46fa809
Daniela <fer.dany.et12@gmail.com>
2022-07-07 00:51:59 -0300

    Aguste de campos y avances js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
-js>
-let ;hojaEstilo=0;
-
-function cambiarCss() {
-    if (hojaEstilo == 0) {
-        document.getElementById('css').href = 'cssprueba/retro.css';
-        hojaEstilo = 1;
+/* const btn = document.getElementById('cambioCss') */
+document.getElementById('cambioCss').addEventListener('click', cambiarCss)
+    function cambiarCss() {     
+        /* const btn = document.getElementById('cambioCss')
+        btn.addEventListener("click", cambiarCss) */
+        var dado = Math.ceil(Math.random()*4);
+        console.log(dado)
+        if (dado == 1) {
+            document.getElementById('estilo').style.href = 'css/estilo.css'
+        }
+        else if (dado == 2) {
+            document.getElementById('futuro').style.href = 'css/estilos-futuro.css'
+        }
+        else if (dado == 3) {
+            console.log('Aun no hay Css ajajaj')
+        }
+        else if (dado == 4) {
+            console.log('No hay un estilo Css')
+        }
     }
-    else if (hojaEstilo == 1) {
-        document.getElementById('css').href = 'cssprueba/futuro.css';
-        hojaEstilo = 2;
-    }
-    else if (hojaEstilo == 2) {
-        document.getElementById('css').href = 'css/estilo.css';
-        hojaEstilo = 0;
-    }
-}
