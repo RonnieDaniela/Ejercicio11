5068f300f661320dfcb83d4541723238425df801
Daniela <fer.dany.et12@gmail.com>
2022-07-10 18:38:59 -0300

    Areglo JAVA

--- js/main.js
+++ js/main.js
@@ -1,20 +1,25 @@
-/* const btn = document.getElementById('cambioCss') */
-document.getElementById('cambioCss').addEventListener('click', cambiarCss) //La paguina queda tenta a que se realice un clik en
-    function cambiarCss() {     
-        /* const btn = document.getElementById('cambioCss')
-        btn.addEventListener("click", cambiarCss) */
-        var dado = Math.ceil(Math.random()*4); //se crea el dado, de numros enteros entre el 1 y 4
-        console.log(dado) //se muestra el numero elegiso
-        if (dado == 1) {
-            document.getElementById('estilo').style.href = 'css/estilo.css' // se selecciona el css
-        }
-        else if (dado == 2) {
-            document.getElementById('futuro').style.href = 'css/estilos-futuro.css' //se selecciona el css
-        }
-        else if (dado == 3) {
-            console.log('Aun no hay Css ajajaj') // Falta un Css
-        }
-        else if (dado == 4) {
-            console.log('No hay un estilo Css') // La idea es que si sale el numero 4, se muestra la paguina sin estilo
-        }
+let hojaEstilo;
+function cambiarCss() {
+    var dado = Math.ceil(Math.random()*4);
+    console.log(dado)
+    if (dado == 1) {
+        document.getElementById('hojaCss').href = 'css/estilo.css';
+        hojaEstilo = dado;
+        console.log('CSS Base')
     }
+    else if (dado == 2) {
+        document.getElementById('hojaCss').href = 'css/estilos-futuro.css';
+        hojaEstilo = dado;
+        console.log('CSS Futuro')
+    }
+    else if (dado == 3) {
+        document.getElementById('hojaCss').href = 'css/estilos-peliculas.css';
+        hojaEstilo = dado;
+        console.log('CSS de Pelis')
+    }
+    else if (dado == 4) {
+        document.getElementById('hojaCss').href = 'css/estilo-sinestilo.css';
+        hojaEstilo = dado;
+        console.log('Sin CSS')
+    }
+}
